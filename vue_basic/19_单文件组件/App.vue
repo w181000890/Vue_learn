9d08260febee9c781201932ec4a47e63420794d4
wm <164078906@qq.com>
2022-10-05 06:23:39 +0800

    vue_test

---
+++ vue_basic/19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+import School from './School'
+import Student from './Student.vue'
+export default { 
+  name:"App",
+  components:{
+    School,
+    Student
+  }
+
+}
+</script>
+
+npm config set registry https://registry.npm.taobao.org
---
+++ vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="demo">
+    <h2>学生名称：{{name}}</h2>
+    <h2>学生年龄：{{age}}</h2>
+  </div>
+</template>
+
+<script>
+export default{
+  name:"Student",
+  data() {
+    return {
+      schoolName: "尚硅谷",
+      adress: "北京",
+    }
+  },  
+}
+</script>
+
