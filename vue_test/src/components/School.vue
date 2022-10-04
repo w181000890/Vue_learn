62281f0179ae01008c504849cd9765696051c260
wm <164078906@qq.com>
2022-10-05 06:46:55 +0800

    提交

---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="demo">
+    <h2>学校名称：{{schoolName}}</h2>
+    <h2>学校地址：{{adress}}</h2>
+    <button @click="showName">点我提示学校名字</button>
+  </div>
+</template>
+
+<script>
+const school = Vue.extend({
+  name:"School",
+  data() {
+    return {
+      schoolName: "尚硅谷",
+      adress: "北京",
+    }
+  },
+  methods:{
+    showName(){
+      alert(this.showName)
+    }
+  }
+})
+// export {school}
+export default school
+</script>
+
+<style>
+ .demo{
+  background-color: orange;
+ }
+</style>
